8ca85b33feac294633e5e8a9d21fb309acfcc773
Instal-entretenimento <60207546+Instal-entretenimento@users.noreply.github.com>
2021-04-19 00:56:44 -0300

    Add files via upload

---
+++ Assets/prefab/players/spawer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawer : MonoBehaviour {
+    public GameObject[] caracter;
+    public Transform[] carac;
+    public Transform playerspawnpoint;
+    void Start () {
+        Instantiate(caracter[caracterS.playerid], playerspawnpoint.position, playerspawnpoint.rotation);
+        
+    }
+}
